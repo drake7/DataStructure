87ca0472d927c3343595b890a6d89c6a14257eec
drake7v <deepak.kumar7v@gmail.com>
2022-06-17 20:09:51 -0700

    changes for business letter

--- src/main/java/arrays/BusinessLetter.java
+++ src/main/java/arrays/BusinessLetter.java
@@ -7,14 +7,27 @@ public class BusinessLetter {
 	public static void main(String...s)
 	{
 		String name;
-		String firstName;
-		String lastName;
+		String firstName="";
+		String lastName="";
 		
 		
 		// 
 		name=JOptionPane.showInputDialog(null,"Please input a customer's name");
 		
+		int[] nums= {1,5,2,8,7};
 		
+		for(int i=0;i<name.length();i++)
+		{
+			
+			
+			if(name.charAt(i)==' ')
+			{
+				firstName=name.substring(0,i);
+				lastName=name.substring(i+1);
+				break;
+			}
+		}
+		JOptionPane.showMessageDialog(null, "Dear"+firstName+" " +lastName+" welcome to class.");
 		
 		
 	}
