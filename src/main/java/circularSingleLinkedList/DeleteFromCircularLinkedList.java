1472ee649b7f0d012a027b1297ed825ed3e6a3c2
drake7v <deepak.kumar7v@gmail.com>
2022-07-21 23:48:53 -0700

    Changes for debugging

---
+++ src/main/java/circularSingleLinkedList/DeleteFromCircularLinkedList.java
@@ -0,0 +1,69 @@
+package circularSingleLinkedList;
+
+public class DeleteFromCircularLinkedList {
+	//in the  first node
+	
+	
+	public void deleteNode(Node head,int location)
+	{
+		//if list is empty
+		
+		if(head==null)
+			return;
+		
+		Node curr=head;
+		Node prev=new Node();
+		if(location==0)
+		{
+			
+			head=head.next;
+			tail.next=head;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//at last Node
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//at specific location
+
+}
--- src/main/java/circularSingleLinkedList/Node.java
+++ src/main/java/circularSingleLinkedList/Node.java
@@ -8,4 +8,8 @@ public Node(int data)
 this.data=data;
 this.next=null;
 }
+public Node()
+{
+this.next=null;
+}
 }
