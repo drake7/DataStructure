6fd697ce37017877d499674e9f3d04327d3a8d12
Drake7 <Deepak.kumar7v@gmail.com>
2022-09-21 13:00:40 -0700

    Changes for print backward Array using recursion.

---
+++ src/main/java/recursion/BackwardArray.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class BackwardArray {
+
+	public static void main(String[] args) {
+		int[] arrayInfo= {1,2,3,4,5,6,7,8,9};
+		printBackwardArray(arrayInfo, 4);
+	}
+	
+	private static void printBackwardArray(int[] arrayInfo,int countPortion) {
+		//if array length is 10 and user ask to print 12 element.
+		if(arrayInfo.length<countPortion)
+		{	System.out.println("Invalid countPortion");
+			return;
+		}
+		if(countPortion<1)
+		{
+			return;
+		}
+		
+		printBackwardArray(arrayInfo, countPortion-1);
+		System.out.println(arrayInfo[arrayInfo.length-countPortion]);
+
+	}
+}
