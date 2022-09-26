b9ca9ccaf658a0d005ae32860fa7a6c141eb52c2
drake7 <deepak.kumar7v@gmail.com>
2022-09-26 11:41:48 -0700

    SHell sort

--- src/main/java/sorting/ShellSort.java
+++ src/main/java/sorting/ShellSort.java
@@ -2,4 +2,44 @@ package sorting;
 
 public class ShellSort {
 
+	public static void main(String[] args) {
+		
+		int[] a= {12,23,1,42,1,521,34};
+		
+		sortingByShell(a);
+		for (int i : a) {
+			System.out.println(i);
+		}
+	}
+	
+	private static  void sortingByShell(int[] arrayForSort) {
+		
+		int length=arrayForSort.length;
+	
+		//now we need to divide this into small shells of same sizes
+		//div is half of the length, and condition for this to finish that div should not reach to 0 
+		//and will split by half every time(not that we are splitting divs not length)
+		for (int div=length/2;div>0;div /=2)
+		{
+			//now apply insertion sort for this div of array(lets assume div is 4 right now)
+			for(int i=div;i<length;i++)
+			{
+				//get the first element of array in the temp
+				int temp=arrayForSort[i];
+				
+				int j;
+				for(j=i;j >= div && arrayForSort[j-div]>temp; j=j-div )
+				{
+					arrayForSort[j]=arrayForSort[j-div];
+					
+				}
+				arrayForSort[j]=temp;
+				
+			}
+			
+		}
+		
+		
+		
+	}
 }
