75037768937be9255392042c01f4142d8110fe62
drake7 <deepak.kumar7v@gmail.com>
2024-12-23 23:53:10 -0800

    Generate Binary String

---
+++ src/main/java/recursion/BinaryStrings.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class BinaryStrings {
+    public static void main(String[] args) {
+        int N = 3; // Length of the binary strings
+        generateBinaryStrings(N, "");
+    }
+
+    private static void generateBinaryStrings(int N, String current) {
+        if (current.length() == N) { // Base case: if the current string is of length N
+            System.out.println(current);
+            return;
+        }
+
+        // Recursive case: Add '0' and recurse
+        generateBinaryStrings(N, current + "0");
+
+        // Recursive case: Add '1' and recurse
+        generateBinaryStrings(N, current + "1");
+    }
+}
